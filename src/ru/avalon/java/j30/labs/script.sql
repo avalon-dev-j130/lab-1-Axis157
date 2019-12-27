3a128cf73ec500dfd44d47b8870b5a3f5e0ad3b7
gurto <gurto@192.168.0.100>
2019-12-27 19:38:49 +0300

    Only Tables, without insert into and FK

--- src/ru/avalon/java/j30/labs/script.sql
+++ src/ru/avalon/java/j30/labs/script.sql
@@ -4,3 +4,60 @@
  * 2. Опишите все необходимые отношения между таблицами.
  * 3. Добавьте в каждую таблицу минимум три записи.
  */
+create table "Order" (
+    id integer,
+    "user" integer,
+    created timestamp,
+    constraint order_id_pk primary key (id)
+);
+
+create table "User"(
+    id integer generated always as identity,
+    email varchar(255),
+    password varchar(255),
+    info integer,
+    role integer,
+    constraint user_email_pk primary key (email),
+    constraint user_id_uq unique(id),
+    constraint user_info_uq unique(info)
+);
+
+create table Roles(
+    id integer generated always as identity,
+    name varchar(255),
+    constraint roles_name_pk primary key (name),
+    constraint roles_id_uq unique (id)
+);
+
+create table Order2Product (
+    order integer,
+    product integer,
+);
+
+create table UserInfo (
+    id integer,
+    name varchar(255) not null,
+    surname varchar(255) not null,
+    constraint userinfo_id_pk primary key (id)
+);
+
+create table Product(
+    id integer generated always as identity,
+    code varchar(255),
+    title varchar(255),
+    supplier integer,
+    initial_price double,
+    retail_value double,
+    constraint product_code_pk primary key (code),
+    constraint product_id_uq unique (id)
+);
+
+create table Supplier (
+    id integer generated always as identity,
+    name varchar(255),
+    address varchar(255) not null,
+    phone varchar(255),
+    representative varchar(255) not null,
+    constraint supplier_name_pk primary key(name),
+    constraint supplier_id_uq unique (id)
+);
